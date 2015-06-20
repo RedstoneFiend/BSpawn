6ccb639b71494f16718af8edc6816bb7071bd559
BuildTools <unconfigured@null.spigotmc.org>
2015-06-20 07:04:14 -0400

    Version 1.0 Initial commit.

---
+++ BSpawn/src/io/github/redstonefiend/bspawn/BSpawn.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.redstonefiend.bspawn;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Chris
+ */
+public class BSpawn extends JavaPlugin implements Listener, CommandExecutor {
+    
+}
